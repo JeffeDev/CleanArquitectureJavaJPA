5329a5fc409fd0d7bf1652aec4d09bc2cfebbf66
JeffeDev <jeffe.info@gmail.com>
2022-07-06 09:42:17 -0300

    Projeto Domain driven Design Arctecture

---
+++ src/main/java/br/com/alura/escola/dominio/aluno/CifradorDeSenha.java
@@ -0,0 +1,9 @@
+package br.com.alura.escola.dominio.aluno;
+
+public interface CifradorDeSenha {
+
+	String cifrarSenha(String senha);
+	
+	boolean validarSenhaCifrada(String senhaCifrada, String senha);
+	
+}
